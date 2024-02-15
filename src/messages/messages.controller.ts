df70d0ee3ec4bd4a808998c34da94942e0882c6b
markpakingan <116990353+markpakingan@users.noreply.github.com>
2024-02-14 19:44:01 -0800

    able to do get and post request completely

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,20 +1,34 @@
 import { Controller, Get, Post, Body, Param, ValidationPipe} from '@nestjs/common';
 import { CreateMessageDTO } from './dtos/create-message.dto'
+import { MessageService } from './messages.service';
+
 
 @Controller('messages')
 export class MessagesController {
 
+    messagesService: MessageService;
+
+
+    constructor() {
+        //Don't do this on real app
+        //use dependency injection
+        
+        this.messagesService = new MessageService();
+    }
+
     @Get()
-    listMessages() {}
+    listMessages() {
+        return this.messagesService.findAll();
+    }
 
     @Post()
     createMessages(@Body(new ValidationPipe()) body: CreateMessageDTO) {
-        console.log(body);
+        return this.messagesService.create(body.content)
     }
 
     @Get('/:id')
     getMessages(@Param("id") id: string) {
-        console.log(id);
+        return this.messagesService.findOne(id)
     }
 
 }
--- src/messages/messages.repository.ts
+++ src/messages/messages.repository.ts
@@ -7,25 +7,27 @@ export class MessagesRepository{
         const contents = await readFile("messages.json", "utf-8");
         const messages = JSON.parse(contents);
 
+        return messages[id]
+
     }
 
 
     async findAll() {
-        const contents = await readFile("message.json", "utf-8"); 
+        const contents = await readFile("messages.json", "utf-8"); 
         const messages = JSON.parse(contents);
 
         return messages;
     }
 
     async create(content: string){
-        const contents = await readFile("message.json", "utf-8");
+        const contents = await readFile("messages.json", "utf-8");
         const messages = JSON.parse(contents);
 
         const id = Math.floor(Math.random() * 999);
 
         messages[id] = { id, content};
 
-        await writeFile("messages.json", JSON.stringify(messages))
+        await writeFile("messages.json", JSON.stringify(messages));
 
     }
 }
